a948b6d179b39b8b2eade4a64078b43a38133500
Karthik Rao <raoka@vmware.com>
2020-07-08 17:50:59 -0400

    wip

--- Assets/Scripts/Behavior.cs
+++ Assets/Scripts/Behavior.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class Behavior : MonoBehaviour
@@ -9,12 +8,17 @@ public class Behavior : MonoBehaviour
     private Vector2 velocity;
 
     private BoxCollider2D boxCollider;
+    
+    private Vector2 screenBounds;
+
 
     // Use this for initialization
     void Start()
     {
         velocity = Vector2.zero;
-        boxCollider = FindObjectOfType<BoxCollider2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        screenBounds =
+            Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
@@ -31,24 +35,30 @@ public class Behavior : MonoBehaviour
 
         velocity.x = Mathf.MoveTowards(velocity.x, speed * movement.x, Time.deltaTime);
         velocity.y = Mathf.MoveTowards(velocity.y, speed * movement.y, Time.deltaTime);
-        
+
         transform.Translate(velocity * Time.deltaTime);
 
         Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
 
         foreach (Collider2D hit in hits)
         {
-            if (hit == boxCollider)
+            if (hit != boxCollider)
             {
-                continue;
-            }
+                ColliderDistance2D distance2D = hit.Distance(boxCollider);
 
-            ColliderDistance2D distance2D = hit.Distance(boxCollider);
-
-            if (distance2D.isOverlapped)
-            {
-                transform.Translate(distance2D.pointA - distance2D.pointB);
+                if (distance2D.isOverlapped)
+                {
+                    transform.Translate(distance2D.pointA - distance2D.pointB);
+                }
             }
         }
+    }
+    
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, -screenBounds.x, screenBounds.x);
+        viewPos.y = Mathf.Clamp(viewPos.y, -screenBounds.y, screenBounds.y);
+        transform.position = viewPos;
     }
 }
